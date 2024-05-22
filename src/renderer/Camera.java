77cc1434cf164d11e8b1705e7ff036ce91caad2f
coralsh286 <coralsh286@gmail.com>
2024-05-22 19:32:49 +0300

    stage 4

--- src/renderer/Camera.java
+++ src/renderer/Camera.java
@@ -1,94 +1,240 @@
+
 package renderer;
+import primitives.*;
+import java.util.MissingResourceException;
 
-import primitives.Point;
-import primitives.Ray;
-import primitives.Vector;
+/**
+ * Camera class represents a camera in the 3D space
+ */
+public class Camera implements Cloneable {
+    private Point p0;
+    private Vector vUp;
+    private Vector vTo;
+    private Vector vRight;
 
-import static primitives.Util.isZero;
+    private double width = 0d;
+    private double height = 0d;
+    private double distance = 0d;
 
-public class Camera {
-
-    private Point _p0;
-    private Vector _vTo;
-    private Vector _vUp;
-    private Vector _vRight;
-    private double _distance = 0.0;
-    private double _width = 0.0;
-    private double _height = 0.0;
-
-    private Camera(){
-
+    /**
+     * Camera getter
+     * @return the location of the camera
+     */
+    public Point getP0() {
+        return p0;
     }
 
-    public Builder getBuilder(){
-        return new Builder(this);
+    /**
+     * Camera getter
+     * @return the up direction of the camera
+     */
+    public Vector getVUp() {
+        return vUp;
     }
 
-    public Ray constructRay(int nX, int nY, int j, int i){
+    /**
+     * Camera getter
+     * @return the direction of the camera
+     */
+    public Vector getVTo() {
+        return vTo;
+    }
 
+    /**
+     * Camera getter
+     * @return the right direction of the camera
+     */
+    public Vector getVRight() {
+        return vRight;
+    }
+
+    /**
+     * Camera getter
+     * @return the width of the view plane
+     */
+    public double getWidth() {
+        return width;
+    }
+
+    /**
+     * Camera getter
+     * @return the height of the view plane
+     */
+    public double getHeight() {
+        return height;
+    }
+
+    /**
+     * Camera getter
+     * @return the distance between the camera and the view plane
+     */
+    public double getDistance() {
+        return distance;
+    }
+
+
+    /**
+     * Camera builder
+     */
+    public static class Builder {
+        private final Camera camera = new Camera();
+
+        /**
+         * Set the location of the camera
+         *
+         * @param p0 the location of the camera
+         */
+        public Builder setLocation(Point p0) {
+            camera.p0 = p0;
+            return this;
+        }
+
+        /**
+         * Set the direction of the camera
+         *
+         * @param vTo the direction of the camera
+         *            (the vector from the camera to the "look-at" point)
+         * @param vUp the up direction of the camera
+         *                     (the vector from the camera to the up direction)
+         */
+        public Builder setDirection(Vector vTo, Vector vUp) {
+            if (Util.isZero(vTo.dotProduct(vUp))) {
+                throw new IllegalArgumentException("vTo and vUp must be orthogonal");
+            }
+            camera.vTo = vTo.normalize();
+            camera.vUp = vUp.normalize();
+            return this;
+        }
+
+        /**
+         * Set the size of the view plane
+         *
+         * @param width  the width of the view plane
+         * @param height the height of the view plane
+         */
+        public Builder setVpSize(double width, double height) {
+            if(width <= 0 || height <= 0) {
+                throw new IllegalArgumentException("width and height must be positive");
+            }
+            camera.width = width;
+            camera.height = height;
+            return this;
+        }
+
+        /**
+         * Set the distance between the camera and the view plane
+         *
+         * @param distance the distance between the camera and the view plane
+         */
+        public Builder setVPDistance(double distance) {
+            if(distance <= 0) {
+                throw new IllegalArgumentException("distance from camera to view must be positive");
+            }
+            camera.distance = distance;
+            return this;
+        }
+
+        /**
+         * Build the camera
+         *
+         * @return the camera
+         */
+        public Camera build() {
+            String className = "Camera";
+            String description = "values not set";
+
+            if(camera.p0 == null)
+                throw new MissingResourceException(className, description, "p0");
+            if(camera.vUp == null)
+                throw new MissingResourceException(className, description, "vUp");
+            if(camera.vTo == null)
+                throw new MissingResourceException(className, description, "vTo");
+            if(camera.width == 0d)
+                throw new MissingResourceException(className, description, "width");
+            if(camera.height == 0d)
+                throw new MissingResourceException(className, description, "height");
+            if(camera.distance == 0d)
+                throw new MissingResourceException(className, description, "distance");
+
+            camera.vRight = camera.vTo.crossProduct(camera.vUp).normalize();
+
+            if(Util.isZero(camera.vTo.dotProduct(camera.vRight)) ||
+                    Util.isZero(camera.vTo.dotProduct(camera.vUp)) ||
+                    Util.isZero(camera.vRight.dotProduct(camera.vUp)))
+                throw new IllegalArgumentException("vTo, vUp and vRight must be orthogonal");
+
+            if(camera.vTo.length() != 1 || camera.vUp.length() != 1 || camera.vRight.length() != 1)
+              throw new IllegalArgumentException("vTo, vUp and vRight must be normalized");
+
+            if(camera.width <= 0 || camera.height <= 0)
+                throw new IllegalArgumentException("width and height must be positive");
+
+            if(camera.distance <= 0)
+                throw new IllegalArgumentException("distance from camera to view must be positive");
+
+            try {
+                return  (Camera)camera.clone();
+            } catch (CloneNotSupportedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    /**
+     * Camera constructor
+     */
+    private Camera() {}
+
+    /**
+     * Builder getter
+     *
+     * @return the camera builder
+     */
+    public static Builder getBuilder() {
+        return new Builder();
+    }
+
+    /**
+     * construct a ray through a pixel
+     *
+     * @param nX the number of pixels in the x direction
+     * @param nY the number of pixels in the y direction
+     * @param j  the x index of the pixel
+     * @param i  the y index of the pixel
+     * @return the ray that passes through the pixel
+     */
+    public Ray constructRay(int nX, int nY, int j, int i) {
         //view plane center Point
-        Point Pc = _p0.add(_vTo.scale(_distance));
+        Point Pc = p0.add(vTo.scale(distance));
 
-        //pixels ratios
-        double Rx = _width / nX;
-        double Ry = _height / nY;
+        //pixels ratios (pixels width and height)
+        double Rx = width / nX;
+        double Ry = height / nY;
 
         //Pij point[i,j] in view-plane coordinates
         Point Pij = Pc;
 
-        //delta values for moving on the view=plane
+        //delta values for moving on the view plane
         double Xj = (j - (nX - 1) / 2d) * Rx;
         double Yi = -(i - (nY - 1) / 2d) * Ry;
 
-        if (!isZero(Xj)) {
-            Pij = Pij.add(_vRight.scale(Xj));
+        //if not on zero coordinates add the delta distance
+        // to the center of point (i,j)
+        // to reach it
+        if (!Util.isZero(Xj)) {
+            Pij = Pij.add(vRight.scale(Xj));
         }
-        if (!isZero(Yi)) {
-            Pij = Pij.add(_vUp.scale(Yi));
+        if (!Util.isZero(Yi)) {
+            Pij = Pij.add(vUp.scale(Yi));
         }
 
-        // vector from camera's eye in the direction of point(i,j) in the viewplane
-        Vector Vij = Pij.subtract(_p0);
+        // vector from camera's eye in the direction of point(i,j) in the view plane
+        Vector Vij = Pij.subtract(p0);
 
-        return new Ray(_p0, Vij);
+        return new Ray(p0, Vij);
     }
 
-    public static class Builder{
 
-        private final Camera _camera;
+}
 
-        public Builder(Camera camera) {
-            _camera = camera;
-        }
-
-        public Builder setVpDistance(double distance) {
-            _camera._distance = distance;
-            return this;
-        }
-
-        public Builder setVpSize(double width, double height) {
-            _camera._width = width;
-            _camera._height =height;
-
-            return this;
-        }
-
-        public Builder setLocation(Point p) {
-            _camera._p0 = p;
-            return this;
-        }
-
-        public Builder setDirection(Vector vTo, Vector vUp) {
-            if(vTo.dotProduct(vUp) != 0)
-                throw new IllegalArgumentException("They are not perpendicular to each other");
-            else{
-                _camera._vTo = vTo.normalize();
-                _camera._vUp = vUp.normalize();
-                return this;
-            }
-        }
-
-    }
-
-    }
 
